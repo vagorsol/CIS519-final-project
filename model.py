b5bf364d70d84bc27a0564c91134da4abf51a4d2
Audrey Yang <audrey.butter0930@gmail.com>
2025-12-09 14:00:08 -0500

    set up class

--- model.py
+++ model.py
@@ -1,8 +1,19 @@
 import numpy as np
 import pandas as pd
 import torch
+from torch import nn
+from sklearn.model_selection import train_test_split
 
-class NewsClassifier:
-    # def _init_():
-    def get_model():
-        return model_instance
+class NewsClassifier(nn.Module):
+    def _init(self, *args, **kwargs):
+        super()._init_(*args, **kwargs)
+    
+    def eval(self):
+        return None
+    
+    def predict(self, batch):
+        return None
+
+    
+def get_model():
+    return NewsClassifier()
