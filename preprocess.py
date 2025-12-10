f4e843a10723cda38f88b7be06d33a398e875c5c
Audrey Yang <audrey.butter0930@gmail.com>
2025-12-09 20:23:40 -0500

    update method so no longer using scrapping

--- preprocess.py
+++ preprocess.py
@@ -1,8 +1,5 @@
 import numpy as np
 import pandas as pd
-import requests
-import re
-from bs4 import BeautifulSoup
 
 def prepare_data(csv_path: str):
   '''
@@ -37,7 +34,7 @@ def from_url(url: str):
 
     Parameters:
     -----------
-      url: the link to read from
+      url: the link to get the title from
 
     Returns:
     -----------
@@ -48,29 +45,15 @@ def from_url(url: str):
   if ".print" in url:
     url = url.replace(".print", "")
 
-  # try to read the link
-  try:
-    response = requests.get(url)
-  except Exception as e:
-    print(e) # this is for debugging
-    return "", 2
-  
-  if response.status_code != 200:
-    return "", 2
-  
   # finds what its news source is  (foxnews - 0 | nbcnews - 1)
   news_source = 0 if "foxnews" in url else 1
 
-  # which html tag to search headline by based on news source 
-  hl_class = "headline speakable" if news_source == 0 else "article-hero-headline__htag"
-
-  # get title (and convert it to a string)
-  soup = BeautifulSoup(response.text, "html.parser")
-  title = str(soup.find("h1", class_= hl_class))
-  
-  title = title.lower() # convert all to lowercase
-  title = title.split(">")[1].split("<")[0] # trim html tags off  
-  title = re.sub(r'[^\w\s]', '', title) # remove punctuation
+  # get the article title
+  title = url.split("/")[-1].split("-") 
+  # if source is nbc, chop off identifier(?) at its end
+  if news_source == 1:
+    title = title[:-1]
+  title = " ".join(title)
 
   return title, news_source
 
